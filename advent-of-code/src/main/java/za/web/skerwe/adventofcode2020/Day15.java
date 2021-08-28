732b109e61dd5f8e92f3a20bbc19c42c2b647bc8
Quintin Henn <quintin.henn@gmail.com>
2021-08-28 22:53:26 +0200

    Add Advent of Code 2020 Day 15 Part 2
    Modified loop to a while for better performance.

--- advent-of-code/src/main/java/za/web/skerwe/adventofcode2020/Day15.java
+++ advent-of-code/src/main/java/za/web/skerwe/adventofcode2020/Day15.java
@@ -18,44 +18,43 @@ public class Day15 {
   private int nthSpokenNumber;
 
   public static void main(String... args) {
-    Day15 day15 = new Day15(2020);
-    int the2020thSpokenNumber = day15.partOne(new int[] { 0, 1, 4, 13, 15, 12, 16 });
+    Day15 day15Part1 = new Day15(2020);
+    int the2020thSpokenNumber = day15Part1.process(new int[] { 0, 1, 4, 13, 15, 12, 16 });
     logger.warn(String.format("The 2020th spoken number is '%s'", the2020thSpokenNumber));
+
+    Day15 day15Part2 = new Day15(30_000_000);
+    int the30000000thSpokenNumber = day15Part2.process(new int[] { 0, 1, 4, 13, 15, 12, 16 });
+    logger.warn(String.format("The 30,000,000th spoken number is '%s'", the30000000thSpokenNumber));
   }
 
   public Day15(int nthSpokenNumber) {
     this.nthSpokenNumber = nthSpokenNumber;
   }
 
-  public int partOne(int[] input) {
-    HashMap<Integer, Integer> memory = new HashMap<>();
+  public int process(int[] input) {
+    HashMap<Integer, Integer> memory = new HashMap<>(nthSpokenNumber);
 
-    logger.info("Initialise memory with all starting numbers ...");
     IntStream inputStream = Arrays.stream(input);
     inputStream.forEach((int currentNumber) -> {
-      this.startTurn += 1;
+      startTurn += 1;
       memory.put(currentNumber, this.startTurn);
     });
 
-    logger.info(String.format("Starting with %s on turn %s", input[input.length - 1], this.startTurn));
-    return playMemoryGame(this.startTurn, memory, input[input.length - 1]);
-  }
+    int lastSpokenNumber = input[input.length - 1];
+    int currentSpokenNumber = -1;
 
-  private int playMemoryGame(int turn, HashMap<Integer, Integer> memory, int lastSpokenNumber) {
-    int currentSpokenNumber = 0;
+    while (startTurn < this.nthSpokenNumber) {
+      currentSpokenNumber = 0;
 
-    Integer lastSpokenTurn = memory.get(lastSpokenNumber);
-    if (lastSpokenTurn != null) {
-      currentSpokenNumber = turn - lastSpokenTurn;
-    }
+      Integer lastSpokenTurn = memory.get(lastSpokenNumber);
+      if (lastSpokenTurn != null) {
+        currentSpokenNumber = startTurn - lastSpokenTurn;
+      }
 
-    memory.put(lastSpokenNumber, turn);
+      memory.put(lastSpokenNumber, startTurn);
 
-    turn += 1;
-    logger.debug(String.format("Number %s was spoken for turn %s", currentSpokenNumber, turn));
-
-    if (turn < nthSpokenNumber) {
-      return playMemoryGame(turn, memory, currentSpokenNumber);
+      startTurn += 1;
+      lastSpokenNumber = currentSpokenNumber;
     }
 
     return currentSpokenNumber;
--- advent-of-code/src/test/java/za/web/skerwe/adventofcode2020/Day15Test.java
+++ advent-of-code/src/test/java/za/web/skerwe/adventofcode2020/Day15Test.java
@@ -7,47 +7,29 @@ public class Day15Test {
 
   private Day15 classUnderTest;
   private static final int THE_2020TH_NUMBER_SPOKEN = 2020;
-  private static final int THE_30000000TH_NUMBER_SPOKEN = 30000000;
+  private static final int THE_30000000TH_NUMBER_SPOKEN = 30_000_000;
 
   @Test
-  void the2020thNumberSpokenExampleTest1() {
+  void the2020thNumberSpokenPart1() {
     classUnderTest = new Day15(Day15Test.THE_2020TH_NUMBER_SPOKEN);
-    assertEquals(436, classUnderTest.partOne(new int[] { 0, 3, 6 }));
-  }
-
-  @Test
-  void the2020thNumberSpokenExampleTest2() {
-    classUnderTest = new Day15(Day15Test.THE_2020TH_NUMBER_SPOKEN);
-    assertEquals(1, classUnderTest.partOne(new int[] { 1, 3, 2 }));
-  }
-
-  @Test
-  void the2020thNumberSpokenExampleTest3() {
-    classUnderTest = new Day15(Day15Test.THE_2020TH_NUMBER_SPOKEN);
-    assertEquals(10, classUnderTest.partOne(new int[] { 2, 1, 3 }));
-  }
-
-  @Test
-  void the2020thNumberSpokenExampleTest4() {
-    classUnderTest = new Day15(Day15Test.THE_2020TH_NUMBER_SPOKEN);
-    assertEquals(27, classUnderTest.partOne(new int[] { 1, 2, 3 }));
-  }
-
-  @Test
-  void the2020thNumberSpokenExampleTest5() {
-    classUnderTest = new Day15(Day15Test.THE_2020TH_NUMBER_SPOKEN);
-    assertEquals(78, classUnderTest.partOne(new int[] { 2, 3, 1 }));
-  }
-
-  @Test
-  void the2020thNumberSpokenExampleTest6() {
-    classUnderTest = new Day15(Day15Test.THE_2020TH_NUMBER_SPOKEN);
-    assertEquals(438, classUnderTest.partOne(new int[] { 3, 2, 1 }));
-  }
-
-  @Test
-  void the2020thNumberSpokenExampleTest7() {
-    classUnderTest = new Day15(Day15Test.THE_2020TH_NUMBER_SPOKEN);
+    // assertEquals(436, classUnderTest.partOne(new int[] { 0, 3, 6 }));
+    // assertEquals(1, classUnderTest.partOne(new int[] { 1, 3, 2 }));
+    // assertEquals(10, classUnderTest.partOne(new int[] { 2, 1, 3 }));
+    // assertEquals(27, classUnderTest.partOne(new int[] { 1, 2, 3 }));
+    // assertEquals(78, classUnderTest.partOne(new int[] { 2, 3, 1 }));
+    // assertEquals(438, classUnderTest.partOne(new int[] { 3, 2, 1 }));
     assertEquals(1836, classUnderTest.partOne(new int[] { 3, 1, 2 }));
+  }
+
+  @Test
+  void the30000000thNumberSpokenPart2() {
+    classUnderTest = new Day15(Day15Test.THE_30000000TH_NUMBER_SPOKEN);
+    // assertEquals(175594, classUnderTest.partOne(new int[] { 0, 3, 6 }));
+    // assertEquals(2578, classUnderTest.partOne(new int[] { 1, 3, 2 }));
+    // assertEquals(3544142, classUnderTest.partOne(new int[] { 2, 1, 3 }));
+    // assertEquals(261214, classUnderTest.partOne(new int[] { 1, 2, 3 }));
+    // assertEquals(6895259, classUnderTest.partOne(new int[] { 2, 3, 1 }));
+    // assertEquals(18, classUnderTest.partOne(new int[] { 3, 2, 1 }));
+    assertEquals(362, classUnderTest.partOne(new int[] { 3, 1, 2 }));
   }
 }
