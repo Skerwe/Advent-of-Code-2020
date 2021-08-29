f2b49da9cbe2021d16ca758eaa64fdc5be7f5c93
Quintin Henn <quintin.henn@gmail.com>
2021-08-29 18:59:36 +0200

    Add Advent of Code 2020 Day 16 Part 2

--- advent-of-code/src/main/java/za/web/skerwe/adventofcode2020/Day16.java
+++ advent-of-code/src/main/java/za/web/skerwe/adventofcode2020/Day16.java
@@ -22,6 +22,8 @@ public class Day16 {
   private HashMap<String, int[][]> ticketData = new HashMap<>();
   private ArrayList<Integer> yourTicket = new ArrayList<>();
   private ArrayList<ArrayList<Integer>> nearbyTickets = new ArrayList<>();
+  private HashMap<Integer, ArrayList<String>> ticketForm = new HashMap<>();
+  private ArrayList<Integer> markFieldFound = new ArrayList<>();
 
   public static void main(String... args) {
     Day16 day16Part1 = new Day16();
@@ -29,6 +31,9 @@ public class Day16 {
     File inputFile = new File("src/main/resources/input-day16.txt");
     int ticketScanningErrorRate = day16Part1.partOne(inputFile.getAbsolutePath());
     logger.warn(String.format("The ticket scanning error rate is '%s'", ticketScanningErrorRate));
+
+    long departureFieldsValue = day16Part1.partTwo(inputFile.getAbsolutePath());
+    logger.warn(String.format("Multiply the six departure field values together is '%s'", departureFieldsValue));
   }
 
   public int partOne(String inputFile) {
@@ -36,11 +41,37 @@ public class Day16 {
     return calculateTicketScanningErrorRate();
   }
 
+  public long partTwo(String inputFile) {
+    buildDataModel(readInputFile(inputFile));
+    calculateTicketScanningErrorRate();
+    buildTicketForm();
+    reduceTicketForm();
+    return multiplyDepartureFieldValuesTogether();
+  }
+
+  private void printTicketForm() {
+    for (int position = 0; position < yourTicket.size(); position++) {
+      String names = "";
+      for (String name : ticketForm.get(position)) {
+        names += ":" + name;
+      }
+      logger.debug(String.format("%d -> %s", position, names));
+    }
+  }
+
+  /**
+   * Part 1: Find all invalid tickets to calculate ticketScanningErrorRate.
+   * 
+   * Part 2: Remove all invalid tickets.
+   * 
+   * @return ticketScanningErrorRate
+   */
   private int calculateTicketScanningErrorRate() {
     int ticketScanningErrorRate = 0;
     ArrayList<ArrayList<Integer>> errorTicketsToRemove = new ArrayList<>();
 
     for (ArrayList<Integer> arrayList : nearbyTickets) {
+
       for (Integer integer : arrayList) {
         boolean validTicketNumber = false;
         for (int[][] dataSet : ticketData.values()) {
@@ -50,15 +81,114 @@ public class Day16 {
             break;
           }
         }
+
         if (!validTicketNumber) {
           ticketScanningErrorRate += integer;
           errorTicketsToRemove.add(arrayList);
-          break;
+        }
+      }
+
+    }
+    logger.warn("Removing " + errorTicketsToRemove.size());
+    nearbyTickets.removeAll(errorTicketsToRemove);
+    logger.warn("Valid tickets: " + nearbyTickets.size());
+    return ticketScanningErrorRate;
+  }
+
+  private void buildTicketForm() {
+    for (String fieldName : ticketData.keySet()) {
+      for (int position = 0; position < yourTicket.size(); position++) {
+        if (determinePosibleTickePositions(ticketData.get(fieldName), position)) {
+          ArrayList<String> fieldNames = ticketForm.get(position);
+          if (fieldNames == null) {
+            fieldNames = new ArrayList<>();
+          }
+          fieldNames.add(fieldName);
+          ticketForm.put(position, fieldNames);
         }
       }
     }
-    nearbyTickets.removeAll(errorTicketsToRemove);
-    return ticketScanningErrorRate;
+  }
+
+  /**
+   * If the postion (column) of each ticket (row) is outside the ranges of the
+   * dataset return false.
+   * 
+   * @param dataSet  the current field data ranges.
+   * @param position the current column.
+   * @return false if the entire row is outside the range of the dataset.
+   */
+  private boolean determinePosibleTickePositions(int[][] dataSet, int position) {
+    boolean validposition = true;
+
+    for (ArrayList<Integer> arrayList : nearbyTickets) {
+
+      if (arrayList.get(position) < dataSet[0][0]
+          || arrayList.get(position) > dataSet[0][1] && arrayList.get(position) < dataSet[1][0]
+          || arrayList.get(position) > dataSet[1][1]) {
+        validposition = false;
+        break;
+      }
+    }
+
+    return validposition;
+  }
+
+  private void reduceTicketForm() {
+    while (checkIfTicketFormStillContainDuplicates()) {
+      printTicketForm();
+      String fieldName = findSingleFieldEntry();
+      removeFieldFromOtherPostions(fieldName);
+      logger.warn("");
+    }
+  }
+
+  private String findSingleFieldEntry() {
+    String foundFieldName = "";
+    for (Integer position : ticketForm.keySet()) {
+      if (ticketForm.get(position).size() == 1 && !markFieldFound.contains(position)) {
+        foundFieldName = ticketForm.get(position).get(0);
+        markFieldFound.add(position);
+      }
+    }
+    return foundFieldName;
+  }
+
+  private void removeFieldFromOtherPostions(String fieldName) {
+    for (Integer position : ticketForm.keySet()) {
+      if (ticketForm.get(position).size() > 1) {
+        ticketForm.get(position).remove(fieldName);
+      }
+    }
+  }
+
+  private boolean checkIfTicketFormStillContainDuplicates() {
+    boolean containsDuplicates = false;
+    for (int position = 0; position < yourTicket.size(); position++) {
+      if (ticketForm.get(position).size() > 1) {
+        containsDuplicates = true;
+        break;
+      }
+    }
+
+    return containsDuplicates;
+  }
+
+  private long multiplyDepartureFieldValuesTogether() {
+    long departureFieldsValue = 1;
+
+    for (Integer position : ticketForm.keySet()) {
+      if (ticketForm.get(position).size() == 1) {
+        String fieldName = ticketForm.get(position).get(0);
+        logger.debug(String.format("%s has position %d", fieldName.toUpperCase(), position + 1));
+        if (fieldName.startsWith("departure")) {
+          logger.info(departureFieldsValue + "*" + yourTicket.get(position));
+          departureFieldsValue *= yourTicket.get(position);
+        }
+      }
+    }
+
+    return departureFieldsValue;
   }
 
   /* BUILDING DATA MODEL METHODS */
@@ -92,8 +222,8 @@ public class Day16 {
 
     }
 
-    logger.debug("yourTicket=" + yourTicket.size());
-    logger.debug("nearbyTickets=" + nearbyTickets.size());
+    logger.info("yourTicket=" + yourTicket.size());
+    logger.info("nearbyTickets=" + nearbyTickets.size());
   }
 
   private void addTicketData(String ticketDataEntry) {
@@ -116,13 +246,10 @@ public class Day16 {
   }
 
   private ArrayList<Integer> parseNumbers(String[] ticketValuesRaw) {
-    String values = "";
     ArrayList<Integer> ticketValues = new ArrayList<>();
     for (String value : ticketValuesRaw) {
       ticketValues.add(Integer.parseInt(value));
-      values += value + ":";
     }
-    logger.debug(values);
     return ticketValues;
   }
 
--- advent-of-code/src/test/java/za/web/skerwe/adventofcode2020/Day16Test.java
+++ advent-of-code/src/test/java/za/web/skerwe/adventofcode2020/Day16Test.java
@@ -2,20 +2,30 @@ package za.web.skerwe.adventofcode2020;
 
 import java.io.File;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class Day16Test {
 
   private Day16 classUnderTest;
+  private File inputFile;
+
+  @BeforeEach
+  void setup() {
+    classUnderTest = new Day16();
+  }
 
   @Test
-  void testPart1() {
-    classUnderTest = new Day16();
-
-    File inputFile = new File("src/test/resources/test-input-day16.txt");
-
+  void testPartOne() {
+    inputFile = new File("src/test/resources/test-input-day16-part1.txt");
     assertEquals(71, classUnderTest.partOne(inputFile.getAbsolutePath()));
+  }
+
+  @Test
+  void testPartTwo() {
+    inputFile = new File("src/test/resources/test-input-day16-part2.txt");
+    assertTrue(0 < classUnderTest.partTwo(inputFile.getAbsolutePath()));
   }
 
 }
